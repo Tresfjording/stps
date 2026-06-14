6b69827fbe9f476ff47db27466ddafd3e17ba99b
Tresfjording <granberg@tresfjording.no>
2026-06-15 00:54:04 +0200

    import base64

--- blabla.py
+++ blabla.py
@@ -9,62 +9,62 @@ import base64
 
 #df = pd.read_excel(file, sheet_name="Hovedtabell", engine="openpyxl")
 
-df = df.rename(columns={df.columns[0]: "Navn"})
-df = df.dropna(subset=["Navn"])
+# df = df.rename(columns={df.columns[0]: "Navn"})
+# df = df.dropna(subset=["Navn"])
 
-columns = df.columns[1:]  # alle kolonner unntatt Navn
-current = 0
+# columns = df.columns[1:]  # alle kolonner unntatt Navn
+# current = 0
 
-fig, ax = plt.subplots(figsize=(10, 6))
-
-
-def draw(col_index):
-    ax.clear()
-
-    col = columns[col_index]
-
-    temp = df[["Navn", col]].dropna()
-    temp[col] = pd.to_numeric(temp[col], errors="coerce")
-    temp = temp.dropna()
-
-    leaderboard = temp.set_index("Navn")[col].sort_values(ascending=True)
-
-    ax.barh(leaderboard.index, leaderboard.values, color="steelblue")
-
-    # legg på tall
-    for i, v in enumerate(leaderboard):
-        ax.text(v + 1, i, str(int(v)), va='center')
-
-    ax.set_title(f"{col}")
-    ax.set_xlabel("Poeng")
-
-    # fjern ramme
-    for spine in ax.spines.values():
-        spine.set_visible(False)
-
-    ax.tick_params(left=False)
-    ax.grid(axis="x")
+# fig, ax = plt.subplots(figsize=(10, 6))
 
 
-def on_key(event):
-    global current
+# def draw(col_index):
+#     ax.clear()
 
-    if event.key == "right":
-        current += 1
-    elif event.key == "left":
-        current -= 1
+#     col = columns[col_index]
 
-    current = current % len(columns)  # loop rundt
+#     temp = df[["Navn", col]].dropna()
+#     temp[col] = pd.to_numeric(temp[col], errors="coerce")
+#     temp = temp.dropna()
 
-    draw(current)
-    fig.canvas.draw()
+#     leaderboard = temp.set_index("Navn")[col].sort_values(ascending=True)
+
+#     ax.barh(leaderboard.index, leaderboard.values, color="steelblue")
+
+#     # legg på tall
+#     for i, v in enumerate(leaderboard):
+#         ax.text(v + 1, i, str(int(v)), va='center')
+
+#     ax.set_title(f"{col}")
+#     ax.set_xlabel("Poeng")
+
+#     # fjern ramme
+#     for spine in ax.spines.values():
+#         spine.set_visible(False)
+
+#     ax.tick_params(left=False)
+#     ax.grid(axis="x")
 
 
-fig.canvas.mpl_connect("key_press_event", on_key)
+# def on_key(event):
+#     global current
+
+#     if event.key == "right":
+#         current += 1
+#     elif event.key == "left":
+#         current -= 1
+
+#     current = current % len(columns)  # loop rundt
+
+#     draw(current)
+#     fig.canvas.draw()
 
 
-draw(current)
-plt.show()
+# fig.canvas.mpl_connect("key_press_event", on_key)
+
+
+# draw(current)
+# plt.show()
 
 # 2. Generate your chart with matplotlib.pyplot
 fig, ax = plt.subplots(figsize=(7, 4))
