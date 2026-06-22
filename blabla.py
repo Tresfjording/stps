bbc8ee89dfcfd622797801d89a74868c43c913f8
Tresfjording <granberg@tresfjording.no>
2026-06-22 15:10:02 +0200

    22.06.2026  - 15:09:59

--- blabla.py
+++ blabla.py
@@ -12,7 +12,7 @@ df = df.rename(columns={df.columns[0]: "Navn"})
 df = df.dropna(subset=["Totalt"])
 
 # 2. Generate your chart with matplotlib.pyplot
-fig, ax = plt.subplots(figsize=(7, 7))
+fig, ax = plt.subplots(figsize=(7, 4))
 ax.bar(df.iloc[:, 0], df.iloc[:, 14], color="#175c5f", width=[0.5])
 ax.set_title("Årsresultat 2026/2027")
 ax.set_xlabel(df.columns[0])
